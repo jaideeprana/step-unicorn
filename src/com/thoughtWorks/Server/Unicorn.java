03a57e08b7df0aa46fcd9fc5e68b6c00833f4789
sandeepsinha <sinha.sandeep01@gmail.com>
2013-04-24 21:48:20 +0530

    Revert "Bipilesh,Sandeep | Changed unicorn.java file to solve the bug (because to get port we had to restart port 3 times.)"
    This reverts commit c8832f9f241ffa4a41e94f39d008f04c3a4d7120.

--- src/com/thoughtWorks/Server/Unicorn.java
+++ src/com/thoughtWorks/Server/Unicorn.java
@@ -2,8 +2,13 @@ package com.thoughtWorks.Server;
 
 public class Unicorn {
     public static void main(String[] args) {
+        try{
             Server server=new Server(9090);
             System.out.println("The Server Begins");
             server.run();
+        }
+        catch(Exception e){
+            System.out.println("connection not found");
+        }
     }
 }
