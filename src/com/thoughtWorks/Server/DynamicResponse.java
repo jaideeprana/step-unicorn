c29aeb0f524a8dfcf3c6e14d4a27d5cb38dd7ff6
Bipilesh <bipilesh.k@gmail.com>
2013-04-25 21:33:14 +0530

    dynamic done

--- src/com/thoughtWorks/Server/DynamicResponse.java
+++ src/com/thoughtWorks/Server/DynamicResponse.java
@@ -1,16 +1,21 @@
 package com.thoughtWorks.Server;
 
 
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class DynamicResponse {
 
-    public void generate(DataOutputStream dataOutputStream) throws IOException {
-        String ipAddress = "10.10.5.126", portInString = "8080", resources = "/forum/login";
+    public void generate(DataOutputStream dataOutputStream, String ipAddress, String portInString, String resources) throws IOException {
         int port = Integer.parseInt(portInString);
         String request = "GET HTTP://" + ipAddress + ":" + portInString + resources + "  HTTP/1.1" + "\n" + "" + "\n" + "";
+
+
         Socket socket = new Socket(InetAddress.getByName(ipAddress), port);
         PrintWriter out = new PrintWriter(socket.getOutputStream());
         out.println(request);
@@ -26,7 +31,7 @@ public class DynamicResponse {
         String fromServer;
 
         while ((fromServer = in.readLine()) != null) {
-            dataOutputStream.writeBytes(fromServer+"\n");
+            dataOutputStream.writeBytes(fromServer + "\n");
         }
         dataOutputStream.flush();
         dataOutputStream.close();
---
+++ test/com/thoughtWorks/Server/DynamicResponseTest.java
@@ -0,0 +1,9 @@
+package com.thoughtWorks.Server;
+
+import org.junit.Test;
+
+public class DynamicResponseTest {
+    @Test
+    public void testName() throws Exception {
+    }
+}
